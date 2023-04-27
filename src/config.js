d42987f09fff677398228c87cbf52e5c7a9fa9c7
rimmi <rimzimranga@gmail.com>
2023-04-27 18:07:46 +0530

    fifthcommit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAYGlsaHu0JtpfXBNHW6o_7yeNv4ahZzSY",
+    authDomain: "chatroom-dadda.firebaseapp.com",
+    projectId: "chatroom-dadda",
+    storageBucket: "chatroom-dadda.appspot.com",
+    messagingSenderId: "35895781331",
+    appId: "1:35895781331:web:e4e6cd1aa144016af03126",
+    measurementId: "G-5ZBBLL7VSP"
+  
+}
